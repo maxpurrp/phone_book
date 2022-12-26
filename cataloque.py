152294cf7d94554c989fe047b0ffd51ca9004a76
maxpurrp <seminm041@gmail.com>
2022-12-26 13:31:19 +0300

    +

--- cataloque.py
+++ cataloque.py
@@ -65,9 +65,14 @@ def save(choise,path):
 def close_prog():
     print("Хотите сохранить справочник?")
     print("yes or no")
-    choise=input()
-    if choise=="yes":
-        save()
+    asq_question=input()
+    if asq_question=="yes":
+        print("Хотите добавить в существующий справочник или создать новый?")
+        print("add or new")
+        choise=input()
+        print("Введите путь для сохранения файла")
+        path=input()
+        save(choise,path)
     exit()
 def add_user(number_books,name,number):
     if name in number_books.keys():
@@ -96,7 +101,7 @@ def help_in_number_books():
     print("delete <name> Позволяет удалить номер телефона")
     print("replace <name> <number> Позволяет заменить номер абонента")
     print("showbook  Позволяет посмотореть содержимое справочника")
-    print("exit  Выход из справочника")
+    print("exit  Выход из справочника c возможностью его сохранения")
     print("help  Печать списка с пояснениями")
     print("save  Сохранить текущую версию справочника")
     print("load_from_file Загрузить файл с контактами")
